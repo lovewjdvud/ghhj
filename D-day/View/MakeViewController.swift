fc41633b7b92a89cd22a96803f948a814828fa3b
lovewjdvud <76860314+lovewjdvud@users.noreply.github.com>
2022-09-03 12:43:50 +0900

    ㄴ

---
+++ D-day/View/MakeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MakeViewController.swift
+//  D-day
+//
+//  Created by Songjeongpyeong on 2022/03/11.
+//
+
+import UIKit
+
+class MakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- simple_schedule/service/testJson.swift
+++
@@ -1,8 +0,0 @@
-//
-//  testJson.swift
-//  simple_schedule
-//
-//  Created by Songjeongpyeong on 2022/03/16.
-//
-
-import Foundation
